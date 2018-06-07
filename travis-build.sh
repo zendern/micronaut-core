3fdcd80ca20da53b6b449e6eda2c97e844b5163f
Graeme Rocher <graeme.rocher@gmail.com>
2018-06-07 16:42:05 +0200

    Run all tests in suite

--- travis-build.sh
+++ travis-build.sh
@@ -7,7 +7,7 @@ git config --global user.email "$GIT_EMAIL"
 git config --global credential.helper "store --file=~/.git-credentials"
 echo "https://$GH_TOKEN:@github.com" > ~/.git-credentials
 
-./gradlew test-suite:test --tests io.micronaut.upload.UploadSpec --no-daemon || EXIT_STATUS=$?
+./gradlew test-suite:test --no-daemon || EXIT_STATUS=$?
 
 
 exit $EXIT_STATUS
